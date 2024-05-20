3fef73b8f9664bfd4a39a291fe25238668f4a5c3
hiwon-lee <leather2790@naver.com>
2024-05-20 20:00:33 +0900

    fix 밀리초 삭제 + 시간 추가

--- frontend/src/components/Timer.jsx
+++ frontend/src/components/Timer.jsx
@@ -5,13 +5,13 @@ export default function Timer(props) {
     return (
         <div className="timer">
             <span className="digits">
+                {("0" + Math.floor((props.time / 3600000) % 24)).slice(-2)}:
+            </span>
+            <span className="digits">
                 {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
             </span>
             <span className="digits">
-                {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}.
-            </span>
-            <span className="digits mili-sec">
-                {("0" + ((props.time / 10) % 100)).slice(-2)}
+                {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}
             </span>
         </div>
     );

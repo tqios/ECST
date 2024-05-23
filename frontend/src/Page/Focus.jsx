64af96b262cf9660b60aa770cd6dc56d4c88953a
hiwon-lee <leather2790@naver.com>
2024-05-23 13:56:20 +0900

    feat 집중 데이터 fetch -> 표 시각화

--- frontend/src/Page/Focus.jsx
+++ frontend/src/Page/Focus.jsx
@@ -3,54 +3,51 @@ import "react-datepicker/dist/react-datepicker.css";
 import MenuNav from "../components/MenuNav.jsx";
 import React, { useEffect, useState } from "react";
 import { CgProfile } from "react-icons/cg";
-import axios from "axios";
 import { Link, useHistory, useLocation } from "react-router-dom";
+import { getRecordConcentrate } from "./Utils/api";
 
 function Focus() {
   const [user, setUser] = useState("로그인 필요");
   const [isLoading, setisLoading] = useState(true);
-  const [study, setStudy] = useState([]);
+  const [concentrateRecord, setConcentrateRecord] = useState([]);
   const history = useHistory();
   const location = useLocation();
 
   useEffect(() => {
     const userEmail = localStorage.getItem("user_email");
     if (userEmail) {
-      fetchData(userEmail);
+      setUser(userEmail);
     } else {
       history.push("/login");
     }
-  }, []);
+  }, [history]);
 
-  const fetchData = async (email) => {
-    try {
-      console.log(email);
+  useEffect(() => {
+    const fetchConcentrateRecord = async () => {
+      try {
+        if (user !== "로그인 필요") {
+          const result = await getRecordConcentrate(user);
+          console.log(result);
+          setConcentrateRecord(result);
+        }
+      } catch (e) {
+        alert("공부기록이 없거나, 사용자가 유용하지 않습니다.");
+        console.log(e.message);
+      }
+    };
 
-      // study_todo 가져오기 위한 axios
-      const response = await axios.get("http://127.0.0.1:8000/api/study/", {
-        params: {
-          email: email,
-        },
-      });
-      setUser(response.data.user);
-
-      console.log(response.data.feeds);
-      await setStudy(response.data.feeds);
-      setisLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    fetchConcentrateRecord();
+  }, [user]);
 
   const Calendar = () => {
     const [selectedDate, setSelectedDate] = useState(new Date());
 
     return (
       <DatePicker
-        dateFormat="yyyy.MM.dd" // 날짜 형태
-        shouldCloseOnSelect={true} // 날짜를 선택하면 datepicker가 자동으로 닫힘
-        minDate={new Date("2000-01-01")} // minDate 이전 날짜 선택 불가
-        maxDate={new Date()} // maxDate 이후 날짜 선택 불가
+        dateFormat="yyyy.MM.dd"
+        shouldCloseOnSelect={true}
+        minDate={new Date("2000-01-01")}
+        maxDate={new Date()}
         selected={selectedDate}
         onChange={(date) => setSelectedDate(date)}
       />
@@ -64,8 +61,7 @@ function Focus() {
           to={{
             pathname: "/home",
             state: {
-              email:
-                location.state?.email || localStorage.getItem("user_email"),
+              email: location.state?.email || localStorage.getItem("user_email"),
             },
           }}
           className="text-5xl font-bold ml-6">
@@ -81,8 +77,7 @@ function Focus() {
             to={{
               pathname: "/my-page",
               state: {
-                email:
-                  location.state?.email || localStorage.getItem("user_email"),
+                email: location.state?.email || localStorage.getItem("user_email"),
               },
             }}>
             {user}
@@ -90,10 +85,8 @@ function Focus() {
         </div>
       </div>
       <hr />
-      {/*메뉴바*/}
       <div className="p-2 bg-sky-300 text-white font-bold">
         <MenuNav />
-        {/*{user}*/}
       </div>
       <hr />
 
@@ -102,7 +95,7 @@ function Focus() {
           날짜 선택 :
           <Calendar />
         </div>
-        <div className="ml-12 mt-5  text-ml">
+        <div className="ml-12 mt-5 text-ml">
           <table className="table-auto w-full border-collapse border border-gray-300 shadow-md rounded-lg">
             <thead>
               <tr className="bg-white">
@@ -112,21 +105,13 @@ function Focus() {
               </tr>
             </thead>
             <tbody>
-              <tr className="bg-white hover:bg-gray-100 text-center">
-                <td className="border border-gray-300 px-4 py-2">5월 20일</td>
-                <td className="border border-gray-300 px-4 py-2">0:46:12</td>
-                <td className="border border-gray-300 px-4 py-2">97%</td>
-              </tr>
-              <tr className="bg-gray-50 hover:bg-gray-100 text-center">
-                <td className="border border-gray-300 px-4 py-2">5월 21일</td>
-                <td className="border border-gray-300 px-4 py-2">1:47:20</td>
-                <td className="border border-gray-300 px-4 py-2">56.5%</td>
-              </tr>
-              <tr className="bg-white hover:bg-gray-100 text-center">
-                <td className="border border-gray-300 px-4 py-2">5월 22일</td>
-                <td className="border border-gray-300 px-4 py-2">0:10:00</td>
-                <td className="border border-gray-300 px-4 py-2">10.1%</td>
-              </tr>
+              {concentrateRecord.map((record, index) => (
+                <tr className="bg-white hover:bg-gray-100 text-center" key={record.concentrate_date}>
+                  <td className="border border-gray-300 px-4 py-2">{record.concentrate_date}</td>
+                  <td className="border border-gray-300 px-4 py-2">{record.concentrate_time}</td>
+                  <td className="border border-gray-300 px-4 py-2">{record.concentrate_average_value}%</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
